d5517fef7c3ed57c1eb70d40a5e7e41f3a9e8deb
Anuraag <anuraagvarmap@gmail.com>
2026-05-06 10:20:40 +0530

    Added project structure and routing setup

---
+++ src/pages/patients/PatientList.jsx
@@ -0,0 +1,9 @@
+function PatientList() {
+  return (
+    <div>
+      <h1>Patients</h1>
+    </div>
+  );
+}
+
+export default PatientList;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Dashboard from "../pages/dashboard/Dashboard";
+import PatientList from "../pages/patients/PatientList";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/patients" element={<PatientList />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
